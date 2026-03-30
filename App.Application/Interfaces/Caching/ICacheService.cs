7f6d4c75e1a9da09328f534246a76e627e1ad656
Erhan Önaldı <onaldierhan@gmail.com>
2026-03-30 09:39:49 +0300

    Cache Service implemented

--- App.Application/Interfaces/Caching/ICacheService.cs
+++ App.Application/Interfaces/Caching/ICacheService.cs
@@ -2,7 +2,7 @@ namespace App.Application.Interfaces.Caching;
 
 public interface ICacheService
 {
-    Task<T> GetAsync<T>(string key);
+    Task<T?> GetAsync<T>(string key);
     Task<T> AddAsync<T>(string key, T value,  TimeSpan? expiration = null);
     Task RemoveAsync(string key);
 }
--- App.Caching/CacheService.cs
+++ App.Caching/CacheService.cs
@@ -1,21 +1,28 @@
 using App.Application.Interfaces.Caching;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace App.Caching;
 
-public class CacheService : ICacheService
+public class CacheService(IMemoryCache memoryCache) : ICacheService
 {
-    public Task<T> GetAsync<T>(string key)
+    public Task<T?> GetAsync<T>(string key)
     {
-        throw new NotImplementedException();
+        return memoryCache.TryGetValue(key, out T cacheValue) ? Task.FromResult( cacheValue ) : Task.FromResult<T>( default(T) );
     }
 
     public Task<T> AddAsync<T>(string key, T value, TimeSpan? expiration = null)
     {
-        throw new NotImplementedException();
+        var cacheOptions = new MemoryCacheEntryOptions()
+        {
+            AbsoluteExpirationRelativeToNow = expiration
+        };
+        memoryCache.Set(key, value, cacheOptions);
+        return Task.FromResult(value);
     }
 
     public Task RemoveAsync(string key)
     {
-        throw new NotImplementedException();
+        memoryCache.Remove(key);
+        return Task.CompletedTask;
     }
 }
